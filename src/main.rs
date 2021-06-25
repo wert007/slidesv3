104a0566c7b2d3a76ba8ccad4fca9b98fd681f15
wert007 <pablohamacher@web.de>
2021-06-25 19:51:02 +0200

    Initialize Reaktor
    Reaktor is the front end of the compiler, which handles source code
    input. This is supposed to be a very light weight interface.

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::io::Write;
+
+use slides::DebugFlags;
+
+fn main() -> Result<(), std::io::Error> {
+    println!("Hello, world!");
+    let args : Vec<String> = std::env::args().collect();
+    let debug_flags = DebugFlags {
+        print_instructions: args.contains(&String::from("-di")),
+        print_current_instruction: args.contains(&String::from("-dci")),
+        print_variable_table: args.contains(&String::from("-dbv")),
+    };
+    if args.len() == 1 || args[1].starts_with('-') {
+        repl(debug_flags)
+    } else {
+        compile(&args[1], debug_flags)
+    }
+}
+
+fn repl(debug_flags: DebugFlags) -> Result<(), std::io::Error> {
+    let mut buf = String::new();
+    loop {
+        buf.clear();
+        print!(">> ");
+        std::io::stdout().flush()?;
+        std::io::stdin().read_line(&mut buf)?;
+        let buf = buf.trim().to_owned();
+        if buf.is_empty() {
+            break;
+        }
+        slides::evaluate(&buf, debug_flags);
+    }
+    Ok(())
+}
+
+fn compile(file_path: &str, debug_flags: DebugFlags) -> Result<(), std::io::Error> {
+    let file = std::fs::read_to_string(file_path)?;
+    slides::evaluate(&file, debug_flags);
+    Ok(())
+}
