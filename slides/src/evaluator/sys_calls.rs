4f8dbc8dfd8247665b0cf69bdef25cfbd6424774
wert007 <pablohamacher@web.de>
2021-09-04 16:02:17 +0200

    Fix Sys Call to print complete Array
    Before print always floored the integer division and lost the last
    element, if the byte count was not a multiple of 4!

--- slides/src/evaluator/sys_calls.rs
+++ slides/src/evaluator/sys_calls.rs
@@ -3,8 +3,8 @@ use super::TypedU64;
 pub fn print(argument: TypedU64, stack: &[u64]) {
     print!("PRINT ");
     if argument.is_pointer {
-        dbg!(stack);
-        let count = stack[argument.value as usize] / 4;
+        // Word size is 4 bytes in this vm. But in doubt we always want do round up.
+        let count = (stack[argument.value as usize] + 3) / 4;
         print!("[ ");
         for i in (argument.value - count..argument.value).rev() {
             print!("{}, ", stack[i as usize]);
