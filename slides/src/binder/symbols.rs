1e0ebc68dac8423390c4db2d9d401cbdf1196a13
wert007 <pablohamacher@web.de>
2021-11-13 22:20:06 +0100

    Add FunctionSymbol
    FunctionSymbols store the FunctionType, label index and name of a
    function, which should be the only things needed to call a function from
    another library.

--- slides/src/binder/symbols.rs
+++ slides/src/binder/symbols.rs
@@ -1,12 +1,35 @@
 use crate::instruction_converter::Program;
 
+use super::{FunctionDeclarationBody, typing::FunctionType};
+
+#[derive(Debug)]
 pub struct Library {
     pub program: Program,
+    pub functions: Vec<FunctionSymbol>,
 }
+
 impl Library {
     pub fn error() -> Self {
         Self {
             program: Program::error(),
+            functions: vec![],
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct FunctionSymbol {
+    pub name: String,
+    pub function_type: FunctionType,
+    pub label_index: u64,
+}
+
+impl From<FunctionDeclarationBody<'_>> for FunctionSymbol {
+    fn from(it: FunctionDeclarationBody) -> Self {
+        Self {
+            name: it.function_name.into(),
+            function_type: it.function_type,
+            label_index: it.function_id,
         }
     }
 }
