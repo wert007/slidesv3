a87cc6855791846045c6b476854fae541ac6bd98
wert007 <pablohamacher@web.de>
2021-11-13 18:22:36 +0100

    Add functions to load libraries from path
    These functions are called by lib('path') functions in the binder. They
    will need to return a value later, so that the libraries can actually be
    used in other places.

--- slides/src/lib.rs
+++ slides/src/lib.rs
@@ -10,9 +10,29 @@ mod parser;
 mod text;
 mod value;
 
+use std::path::Path;
+
 use crate::{diagnostics::DiagnosticBag, text::SourceText};
 
 pub use debug::DebugFlags;
+
+pub fn load_library_from_path<P>(path: P) where P: AsRef<Path> {
+    let path = path.as_ref().to_owned();
+    let source_code = std::fs::read_to_string(&path).unwrap();
+    let file_name = path.file_name().unwrap().to_string_lossy();
+    load_library(&source_code, &file_name, DebugFlags::default())
+}
+
+pub fn load_library(input: &str, file_name: &str, debug_flags: DebugFlags) {
+    let source_text = SourceText::new(input, file_name);
+    let mut diagnostic_bag = DiagnosticBag::new(&source_text);
+    let result = instruction_converter::convert(&source_text, &mut diagnostic_bag, debug_flags);
+    // let result = binder::bind(input, &mut diagnostic_bag);
+    if diagnostic_bag.has_errors() || !debug_flags.run_program {
+        diagnostic_bag.flush_to_console();
+        return;
+    }
+}
 
 pub fn evaluate(input: &str, file_name: &str, debug_flags: DebugFlags) {
     let source_text = SourceText::new(input, file_name);
